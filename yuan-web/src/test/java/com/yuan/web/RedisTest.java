d26b61827b38ed71e8adc85108968d492e2da126
YUAN <YUAN@EDZ-20170117BUU>
2017-12-12 14:43:14 +0800

    mq

--- yuan-web/src/test/java/com/yuan/web/RedisTest.java
+++ yuan-web/src/test/java/com/yuan/web/RedisTest.java
@@ -16,32 +16,32 @@ import redis.clients.jedis.Jedis;
 public class RedisTest extends AbstractTest{
 
 	public static void main(String[] args) {
-		Jedis jedis = new Jedis("localhost");
+		Jedis jedis = new Jedis("192.168.56.1");
 		 System.out.println("连接成功");
         //查看服务是否运行
         System.out.println("服务正在运行: "+jedis.ping());
-        
-        jedis.set("sss", "2222");
-        
-        jedis.lpush("list", "l1");
-        jedis.lpush("list", "l2");
-     // 获取数据并输出
-        Set<String> keys = jedis.keys("*"); 
-        Iterator<String> it=keys.iterator() ;   
-        while(it.hasNext()){   
-            String key = it.next();   
-            System.out.println(key);   
-        }
-        
-        List<String> l = jedis.lrange("list", 0, 2);
-        for(int i=0; i<l.size(); i++) {
-            System.out.println("列表项为: "+l.get(i));
-        }
-        
-        Jedis je = new Jedis("redis.milibx.com");
-        je.auth("mili_bxbb_123");
-        System.out.println(je.ping());
-        je.quit();
+//        
+//        jedis.set("sss", "2222");
+//        
+//        jedis.lpush("list", "l1");
+//        jedis.lpush("list", "l2");
+//     // 获取数据并输出
+//        Set<String> keys = jedis.keys("*"); 
+//        Iterator<String> it=keys.iterator() ;   
+//        while(it.hasNext()){   
+//            String key = it.next();   
+//            System.out.println(key);   
+//        }
+//        
+//        List<String> l = jedis.lrange("list", 0, 2);
+//        for(int i=0; i<l.size(); i++) {
+//            System.out.println("列表项为: "+l.get(i));
+//        }
+//        
+//        Jedis je = new Jedis("redis.milibx.com");
+//        je.auth("mili_bxbb_123");
+//        System.out.println(je.ping());
+//        je.quit();
 	}
 	@Autowired
 	private RedisService redisService;
